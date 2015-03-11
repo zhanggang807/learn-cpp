68acd1abcbf80ccdf91e41134e0fd05a1a0e9d9c
Dean <245405244@qq.com>
2015-03-11 22:39:59 +0800

    add 1-1.cpp

---
+++ 1-1.cpp
@@ -0,0 +1,25 @@
+using namespace std;
+#include <iostream>
+
+int result(int, int);
+const int k = 2;
+
+struct Point{
+	int x, y;
+};
+
+int main(){
+	int z(0), b(50);
+	Point a;
+	cout << "输入两个整数(以空格键区分):";
+	cin >> a.x >> a.y;
+	z = (a.x + a.y) * k;
+	z = result(z, b);
+	cout << "计算结果如下:" << endl;
+	cout << "((" << a.x << " + " << a.y << " ) * " << k << " ) + " << b << " = " << z << endl;
+	return 0;
+}
+
+int result(int a, int b){
+	return a + b;	
+}
