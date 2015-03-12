67143797e6c07212da72a86e18ba39e7cd807222
zhanggang <zhanggang@mapbar.com>
2015-03-12 10:53:11 +0800

    add overload-1.2.cpp

---
+++ overload-1.2.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+#include <iostream>
+
+int max(int, int);
+int max(int, int, int);
+
+void main(){
+    cout << max(35, 25) << "," << max(25, 39, 35) << endl;
+}
+
+int max(int m1, int m2){
+    return m1 > m2 ? m1 : m2;
+}
+
+int max(int m1, int m2, int m3){
+    int t = max(m1, m2);
+    return max(t, m3);
+}
