be1badb3e4e57197c7dc57da5ba0b5565e6d8be7
zhanggang <zhanggang@mapbar.com>
2015-03-12 11:46:08 +0800

    add allocation-1.3.cpp, remove test

---
+++ allocation-1.3.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+#include <iostream>
+
+int main(){
+    double *p;
+    p = new double [3];
+
+    for (int i = 0; i < 3; i++){
+        cin >> *(p + 1);
+    }
+
+    for (int i = 0; i < 3; i++){
+        cout << *(p + 1) << " ";
+    }
+    cout << endl;
+
+    delete p;
+}
