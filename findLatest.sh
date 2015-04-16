8485e6a2dd98011ad617c28ceced4ba78cc7eb5d
zhanggang <zhanggang@mapbar.com>
2015-04-16 13:34:38 +0800

    mv class define from .cpp to .h

--- findLatest.sh
+++ findLatest.sh
@@ -1,1 +1,1 @@
-ll | awk '{print $9}' | awk -F '[-]' '{print $2}' | awk -F '[.]' '{print $1"."$2}' | sort -r
+ll | awk '{print $9}' | awk -F '[-]' '{print $2}' | awk -F '[.]' '{print $1"."$2}' | sort
